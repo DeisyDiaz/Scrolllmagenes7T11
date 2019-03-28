f9f2cea634ad26ccc9523796d7e27be152f9de41
Tesoem <Tesoem@ISC-Tesoem-iMac.local>
2019-03-28 11:12:12 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/itics/diana/scrolllmagenes7t11/MainActivity.java
@@ -0,0 +1,14 @@
+
+package mx.edu.tesoem.itics.diana.scrolllmagenes7t11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
